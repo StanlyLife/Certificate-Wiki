314515e5e8b51b32894f317995f596adccad0bb6
Stian have <stianhave@hotmail.com>
2020-05-22 01:12:01 +0200

    Added ModelValidation helpers

--- Certificate-Wiki/Controllers/AuthController.cs
+++ Certificate-Wiki/Controllers/AuthController.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Certificate_Wiki.Models;
+using Certificate_Wiki.Models.LoginRegister;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Certificate_Wiki.Controllers {
 
 	public class AuthController : Controller {
+		private readonly UserManager<CertificateUser> userManager;
 
-		public AuthController() {
+		public AuthController(UserManager<CertificateUser> UserManager) {
+			userManager = UserManager;
 		}
 
 		[Route("login")]
@@ -17,8 +21,23 @@ namespace Certificate_Wiki.Controllers {
 			return View();
 		}
 
+		[HttpGet]
 		[Route("Register")]
 		public IActionResult register() {
+			return View();
+		}
+
+		[HttpPost]
+		[Route("Register")]
+		public IActionResult register(RegisterModel model) {
+			if (!ModelState.IsValid) {
+				Console.WriteLine("modelstate failed"); //Remove
+				return View(model);
+			}
+
+			var user = userManager.FindByEmailAsync(model.Email);
+			if (user != null) { ModelState.AddModelError("Email", "User Already exists"); return View(model); }
+
 			return View();
 		}
 
--- Certificate-Wiki/Models/LoginRegister/RegisterModel.cs
+++ Certificate-Wiki/Models/LoginRegister/RegisterModel.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 namespace Certificate_Wiki.Models.LoginRegister {
 
 	public class RegisterModel {
+		//These variables does not respect the IdentityUser options
+		[Required]
+		[MinLength(3, ErrorMessage = "Email must be entered")]
+		[MaxLength(64, ErrorMessage = "Email must be less than 64 characters")]
 		public string Email { get; set; }
 
+		[MinLength(6, ErrorMessage = "Password must excede 5 characters")]
+		[MaxLength(64, ErrorMessage = "Password must be less than 64 characters")]
 		[DataType(DataType.Password)]
 		public string Password { get; set; }
 
