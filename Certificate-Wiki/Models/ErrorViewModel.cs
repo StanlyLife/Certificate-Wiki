594c91cfbde05c104bab124870b52348d633fa1a
Stian have <stianhave@hotmail.com>
2020-05-21 19:18:41 +0200

    initial commit for vs2019 project template

---
+++ Certificate-Wiki/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Certificate_Wiki.Models {
+	public class ErrorViewModel {
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
