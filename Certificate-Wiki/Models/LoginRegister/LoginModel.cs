be72cdfa3cdfc22ec0170db72846fcd64162982a
Stian have <stianhave@hotmail.com>
2020-05-23 20:31:59 +0200

    Added loginModel Model will be used to bind properties from login view to auth controller

---
+++ Certificate-Wiki/Models/LoginRegister/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.LoginRegister {
+
+	public class LoginModel {
+
+		[NotNull]
+		public string Email { get; set; }
+
+		[NotNull]
+		public string Password { get; set; }
+	}
+}
