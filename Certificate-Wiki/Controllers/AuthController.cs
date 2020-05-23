c5aad709f20897137f632d87ffd8d24a355f1242
Stian have <stianhave@hotmail.com>
2020-05-23 20:28:39 +0200

    Added registration logic

--- Certificate-Wiki/Controllers/AuthController.cs
+++ Certificate-Wiki/Controllers/AuthController.cs
@@ -6,6 +6,11 @@ using Certificate_Wiki.Models;
 using Certificate_Wiki.Models.LoginRegister;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
 
 namespace Certificate_Wiki.Controllers {
 
@@ -29,16 +34,34 @@ namespace Certificate_Wiki.Controllers {
 
 		[HttpPost]
 		[Route("Register")]
-		public IActionResult register(RegisterModel model) {
+		public async Task<IActionResult> registerAsync(RegisterModel model) {
 			if (!ModelState.IsValid) {
 				Console.WriteLine("modelstate failed"); //Remove
 				return View(model);
 			}
 
-			var user = userManager.FindByEmailAsync(model.Email);
+			var user = await userManager.FindByEmailAsync(model.Email);
 			if (user != null) { ModelState.AddModelError("Email", "User Already exists"); return View(model); }
 
-			return View();
+			//Register user
+			user = new CertificateUser {
+				Id = Guid.NewGuid().ToString(),
+				UserName = model.Email, /*Username cannot be null in Identity*/
+				Email = model.Email
+			};
+
+			var result = await userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded) {
+				foreach (var error in result.Errors.ToList())
+					ModelState.AddModelError("All", error.Description.ToString());
+				return View(model);
+			}
+
+			return View("Success");
+			//Email confirmation
+
+			//Send link to email
 		}
 
 		[Route("Forgotpassword")]
