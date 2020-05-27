625b1320e84ad898cb33fd8f61571c1ddee99522
Stian have <stianhave@hotmail.com>
2020-05-27 03:17:53 +0200

    Changed route

--- Certificate-Wiki/Controllers/SearchController.cs
+++ Certificate-Wiki/Controllers/SearchController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Certificate_Wiki.Controllers
-{
-    public class SearchController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+namespace Certificate_Wiki.Controllers {
+
+	public class SearchController : Controller {
+
+		[Route("Search")]
+		public IActionResult Index() {
+			return View();
+		}
+	}
 }
