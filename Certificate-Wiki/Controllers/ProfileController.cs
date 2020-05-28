5b9f143d7fcee914037cf3cae70f19fd889fe9ed
Stian have <stianhave@hotmail.com>
2020-05-28 16:11:14 +0200

    Initial commit for profile controller

---
+++ Certificate-Wiki/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Certificate_Wiki.Controllers {
+
+	public class ProfileController : Controller {
+
+		[Route("Profile")]
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
