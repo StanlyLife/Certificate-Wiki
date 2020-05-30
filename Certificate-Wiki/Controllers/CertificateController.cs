9fcc786e5fe70f8edc603d15e960fc0cdde27b4c
Stian have <stianhave@hotmail.com>
2020-05-30 19:43:30 +0200

    Added controller for handling certificate logic

---
+++ Certificate-Wiki/Controllers/CertificateController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Certificate_Wiki.Controllers {
+
+	public class CertificateController : Controller {
+
+		public IActionResult Edit() {
+			return View();
+		}
+	}
+}
