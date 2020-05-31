1f6b44f1613f60f6ca5999c4f7c62620edbc2fc5
Stian have <stianhave@hotmail.com>
2020-05-31 05:15:56 +0200

    Added profile logic For simplicity's sake it would be easier to split profile and user, however as it breaks normalization rulesi am uncertain

--- Certificate-Wiki/Controllers/ProfileController.cs
+++ Certificate-Wiki/Controllers/ProfileController.cs
@@ -2,20 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Certificate_Wiki.Models;
+using Certificate_Wiki.Models.Profile;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Certificate_Wiki.Controllers {
 
 	public class ProfileController : Controller {
+		private readonly UserManager<CertificateUser> userManager;
+
+		public ProfileController(UserManager<CertificateUser> userManager) {
+			this.userManager = userManager;
+		}
 
 		[Route("Profile")]
 		public IActionResult Index() {
 			return View();
 		}
 
+		[HttpGet]
+		[Authorize]
 		[Route("Profile/edit")]
-		public IActionResult Edit() {
+		public async Task<IActionResult> EditAsync() {
+			//var Profile = await userManager.FindByEmailAsync(User.Identity.Name);
 			return View();
+		}
+
+		[ValidateAntiForgeryToken]
+		[HttpPost]
+		[Authorize]
+		[Route("Profile/edit")]
+		public async Task<IActionResult> EditAsync(EditProfileModel model) {
+			//TODO
+			//Remove CW from single-line if
+			if (!ModelState.IsValid) { Console.WriteLine("Modelstate invalid"); return View(model); }
+
+			var Profile = await userManager.FindByEmailAsync(User.Identity.Name);
+
+			//Update database
+			Profile.FirstName = model.FirstName;
+			Profile.LastName = model.LastName;
+			Profile.Website = model.Website;
+			Profile.Occupation = model.Occupation;
+			Profile.Country = model.Country;
+
+			await userManager.UpdateAsync(Profile);
+
+			Console.WriteLine("Update success");
+
+			return View(model);
 		}
 	}
 }
