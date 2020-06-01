e62fd0603add7971450bad952b99fd50c8a40dd8
Stian have <stianhave@hotmail.com>
2020-06-01 18:57:46 +0200

    Added delete confirmation Added alert and confirm delete button

---
+++ Certificate-Wiki/wwwroot/js/DeleteBtn.js
@@ -0,0 +1,10 @@
+﻿const button = document.querySelector(".delete-btn");
+const confirmButton = document.querySelector(".delete-confirm");
+
+button.addEventListener("click",deleteBtn);
+
+function deleteBtn() {
+	alert("Do you want to delete your account permanently?");
+	button.style.display = "none";
+	confirmButton.style.display = "block";
+}
