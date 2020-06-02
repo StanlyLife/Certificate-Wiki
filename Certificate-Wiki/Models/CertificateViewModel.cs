2078fefd36f621e5aa96e79046e118e11ee8c82b
Stian have <stianhave@hotmail.com>
2020-06-02 19:25:05 +0200

    Initital commit for implementation: added ViewModel The model's main purpose is to parse Certificate information from controller to view

---
+++ Certificate-Wiki/Models/CertificateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models {
+
+	public class CertificateViewModel {
+		public IEnumerable<Certificates> certificateList { get; set; }
+	}
+}
