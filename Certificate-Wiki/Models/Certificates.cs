ddeaafeae0c1bf742181a8fb996b666bb308b635
Stian have <stianhave@hotmail.com>
2020-06-02 06:11:40 +0200

    Implemented Certificate model

--- Certificate-Wiki/Models/Certificates.cs
+++ Certificate-Wiki/Models/Certificates.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Certificate_Wiki.Models {
 
 	public class Certificates {
+
 		//TODO
 		//Add model requirements
+		public Certificates() {
+		}
 
 		[Key]
-		public string CertificateId { get; set; }
+		public int CertificateId { get; set; }
 
 		[ForeignKey("CertificateUser")]
 		public string UserFk { get; set; }
