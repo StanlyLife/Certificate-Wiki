e17dc30268d0ac86472fa12313a6573bc035b795
Stian have <stianhave@hotmail.com>
2020-06-02 06:12:27 +0200

    Migrated CertificateContext

--- Certificate-Wiki/Migrations/20200602035418_certificates.Designer.cs
+++ Certificate-Wiki/Migrations/20200602035418_certificates.Designer.cs
@@ -4,14 +4,16 @@ using Certificate_Wiki.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Certificate_Wiki.Migrations
 {
     [DbContext(typeof(CertificateDbContext))]
-    partial class CertificateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200602035418_certificates")]
+    partial class certificates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Certificate-Wiki/Migrations/20200602035418_certificates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Certificate_Wiki.Migrations
+{
+    public partial class certificates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CertificateContext",
+                columns: table => new
+                {
+                    CertificateId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserFk = table.Column<string>(nullable: true),
+                    CertificateName = table.Column<string>(nullable: true),
+                    Subject = table.Column<string>(nullable: true),
+                    Admissioner = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CertificateFile = table.Column<byte[]>(nullable: true),
+                    CertificateUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CertificateContext", x => x.CertificateId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CertificateContext");
+        }
+    }
+}
