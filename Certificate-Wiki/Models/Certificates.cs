eb0760ace4907d05cc5d633c38196d23c3175a66
Stian have <stianhave@hotmail.com>
2020-06-02 04:45:44 +0200

    Added Certificate model

---
+++ Certificate-Wiki/Models/Certificates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models {
+
+	public class Certificates {
+		//TODO
+		//Add model requirements
+
+		[Key]
+		public string CertificateId { get; set; }
+
+		[ForeignKey("CertificateUser")]
+		public string UserFk { get; set; }
+
+		public string CertificateName { get; set; }
+		public string Subject { get; set; }
+		public string Admissioner { get; set; }
+		public string Description { get; set; }
+
+		//File OR Url
+		public byte[] CertificateFile { get; set; }
+
+		public string CertificateUrl { get; set; }
+	}
+}
