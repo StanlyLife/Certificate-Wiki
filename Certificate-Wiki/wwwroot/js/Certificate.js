b26542fe100af5a010d778f6d7da77b30385c62a
Stian have <stianhave@hotmail.com>
2020-06-02 19:27:58 +0200

    Implemented hide and show functionality based on radio buttons. The purpose of this script is to make the UX better for the user and force him/her to choose one or another

--- Certificate-Wiki/wwwroot/js/Certificate.js
+++ Certificate-Wiki/wwwroot/js/Certificate.js
@@ -1,1 +1,19 @@
-﻿
+﻿const file = document.querySelector("#file");
+const url = document.querySelector("#url");
+
+const fileUpload = document.querySelector(".file-upload");
+const urlUpload = document.querySelector(".url-upload");
+
+file.addEventListener("change", CheckSelected);
+url.addEventListener("change", CheckSelected);
+
+function CheckSelected() {
+	console.log("test");
+	if (file.checked) {
+		fileUpload.style.display = "block";
+		urlUpload.style.display = "none";
+	} else if (url.checked) {
+		urlUpload.style.display = "block";
+		fileUpload.style.display = "none";
+	}
+}
