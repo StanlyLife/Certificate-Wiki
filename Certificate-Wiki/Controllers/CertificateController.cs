64cee7e45e4dcef3340fac9c36d8ba7c521173e7
Stian have <stianhave@hotmail.com>
2020-06-03 22:38:42 +0200

    Implemented file upload support for certificate When uploading a certificate to the database you can now upload an image or pdf aswell

--- Certificate-Wiki/Controllers/CertificateController.cs
+++ Certificate-Wiki/Controllers/CertificateController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Certificate_Wiki.Interface;
 using Certificate_Wiki.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,9 +27,6 @@ namespace Certificate_Wiki.Controllers {
 			CertificateViewModel certificates = new CertificateViewModel();
 			CertificateUser user = await userManager.FindByNameAsync(User.Identity.Name);
 			certificates.certificateList = CertificateHandler.GetByUserId(user.Id);
-			if (certificates.certificateList.Count() < 1) {
-				certificates.hasCertificates = false;
-			}
 			return View(certificates);
 		}
 
@@ -39,16 +38,43 @@ namespace Certificate_Wiki.Controllers {
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		[Authorize]
-		public IActionResult Upload(Certificates model) {
+		public IActionResult Upload(Certificates model, [FromForm(Name = "file")] IFormFile file) {
 			//TODO
 			//Change subject to a <select> stored in database
-			//Check for file or url
+			//Add modelvalidators
+			if (file != null) {
+				//file upload
+				model.CertificateUrl = null;
+				model.CertificateFile = FileToBytes(file);
+			} else if (model.CertificateUrl != null) {
+				//url upload
+				model.CertificateFile = null;
+			}
+
+			if (model.CertificateUrl == null && model.CertificateFile == null) {
+				ModelState.AddModelError("All", "You must upload or link to a certificate image");
+			}
+
+			if (model.CertificateUrl != null && model.CertificateFile != null) {
+				ModelState.AddModelError("All", "You cannot upload certificate and link a certificate");
+			}
+
 			if (!ModelState.IsValid) { return View(model); }
 
 			model.UserFk = userManager.GetUserId(User);
 			CertificateHandler.Create(model);
 
 			return RedirectToAction("user");
+		}
+
+		public byte[] FileToBytes(IFormFile file) {
+			MemoryStream ms = new MemoryStream();
+			file.CopyTo(ms);
+			var memoryStream = ms.ToArray();
+			ms.Close();
+			ms.Dispose();
+
+			return memoryStream;
 		}
 	}
 }
