5f648740bd6b4431eadec4a406ce13a100a82957
Stian have <stianhave@hotmail.com>
2020-06-04 19:25:11 +0200

    Added logic to edit certificate

--- Certificate-Wiki/Controllers/CertificateController.cs
+++ Certificate-Wiki/Controllers/CertificateController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Certificate_Wiki.Interface;
+using Certificate_Wiki.Migrations;
 using Certificate_Wiki.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +22,11 @@ namespace Certificate_Wiki.Controllers {
 			this.userManager = userManager;
 		}
 
+		[HttpGet]
+		public IActionResult ViewCertificate() {
+			return View();
+		}
+
 		[Authorize]
 		[HttpGet]
 		public async Task<IActionResult> UserAsync(CertificateViewModel model) {
@@ -31,14 +37,21 @@ namespace Certificate_Wiki.Controllers {
 		}
 
 		[HttpGet]
-		public IActionResult Upload() {
-			return View();
+		[Route("Certificate/e/{id:int?}")]
+		public async Task<IActionResult> UploadAsync(int? id) {
+			Certificates certificate = new Certificates();
+			if (id.HasValue && await AuthorizeOwnerAsync(id.Value)) {
+				Console.WriteLine(id);
+				certificate = CertificateHandler.GetById(id.Value);
+			}
+			return View(certificate);
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
+		[Route("Certificate/e/{id:int?}")]
 		[Authorize]
-		public IActionResult Upload(Certificates model, [FromForm(Name = "file")] IFormFile file) {
+		public async Task<IActionResult> UploadAsync(Certificates model, [FromForm(Name = "file")] IFormFile file, int? id) {
 			//TODO
 			//Change subject to a <select> stored in database
 			//Add modelvalidators
@@ -61,10 +74,31 @@ namespace Certificate_Wiki.Controllers {
 
 			if (!ModelState.IsValid) { return View(model); }
 
+			//If the user is updating a certificate and not creating a new one
+			if (id.HasValue && await AuthorizeOwnerAsync(id.Value)) {
+				//TODO
+				//If certificate is not uploaded, but certificate is present in DB, do not throw error
+				model.CertificateId = id.Value;
+				model.UserFk = userManager.GetUserId(User);
+				await TryUpdateModelAsync(model);
+				CertificateHandler.Update(model);
+				return RedirectToAction("user");
+			}
+
 			model.UserFk = userManager.GetUserId(User);
 			CertificateHandler.Create(model);
 
 			return RedirectToAction("user");
+		}
+
+		public async Task<bool> AuthorizeOwnerAsync(int id) {
+			var certificateAuthor = await CertificateHandler.GetAuthorByIdAsync(id);
+			var user = await userManager.FindByNameAsync(User.Identity.Name);
+
+			if (certificateAuthor.Email == user.Email) { return true; }
+			Console.WriteLine("not author");
+
+			return false;
 		}
 
 		public byte[] FileToBytes(IFormFile file) {
