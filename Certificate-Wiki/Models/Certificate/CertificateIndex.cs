94fd2bae1e9882a79b08aa756e6d299455b8221a
Stian have <stianhave@hotmail.com>
2020-06-13 16:21:55 +0200

    Added Certificate Index Layout This page is meant to display a specific certificate

---
+++ Certificate-Wiki/Models/Certificate/CertificateIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.Certificate {
+
+	public class CertificateIndex {
+		public Certificates Certificate { get; set; }
+
+		public bool IsPrivate { get; set; }
+		public bool IsOwner { get; set; }
+
+		public CertificateUser CertificateOwner { get; set; }
+
+		public byte[] CertificateFile { get; set; }
+		public string CertificateUrl { get; set; }
+	}
+}
