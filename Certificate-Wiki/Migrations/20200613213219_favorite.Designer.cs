49e6fc2714447b6704fa9b7e58f4feed3e5185ad
Stian have <stianhave@hotmail.com>
2020-06-13 23:44:31 +0200

    Added migrations for "favorite" feature

--- Certificate-Wiki/Migrations/20200613213219_favorite.Designer.cs
+++ Certificate-Wiki/Migrations/20200613213219_favorite.Designer.cs
@@ -4,14 +4,16 @@ using Certificate_Wiki.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Certificate_Wiki.Migrations
 {
     [DbContext(typeof(CertificateDbContext))]
-    partial class CertificateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200613213219_favorite")]
+    partial class favorite
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
