7fde520b4408ae66a7a5bfc9bdbb8d7cf73aa198
Stian have <stianhave@hotmail.com>
2020-06-13 23:45:00 +0200

    Added model for favorite certificates

---
+++ Certificate-Wiki/Models/Certificate/FavoriteCertificate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.Certificate {
+
+	public class FavoriteCertificate {
+
+		[Key]
+		public int Id { get; set; }
+
+		[NotNull]
+		[ForeignKey("Certificates")]
+		public int certificateId { get; set; }
+
+		[NotNull]
+		[ForeignKey("CertificateUser")]
+		public string UserId { get; set; }
+	}
+}
