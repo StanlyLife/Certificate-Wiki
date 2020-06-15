4a3865e63afd0c75332bf7483e836186e199ba67
Stian have <stianhave@hotmail.com>
2020-06-15 21:01:49 +0200

    Implemented search logic with page results

--- Certificate-Wiki/Controllers/SearchController.cs
+++ Certificate-Wiki/Controllers/SearchController.cs
@@ -1,16 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Threading.Tasks;
+using Certificate_Wiki.Interface;
+using Certificate_Wiki.Models;
+using Certificate_Wiki.Models.Search;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Certificate_Wiki.Controllers {
 
 	public class SearchController : Controller {
+		private readonly UserManager<CertificateUser> userManager;
+		private readonly ICertificateHandler certificateHandler;
 
-		[Route("Search")]
-		public IActionResult Index() {
-			return View();
+		public SearchController(UserManager<CertificateUser> userManager, ICertificateHandler certificateHandler) {
+			this.userManager = userManager;
+			this.certificateHandler = certificateHandler;
+		}
+
+		//TODO
+		//Fix url search bug
+		private int resultsPerPage = 2;
+
+		[Route("Search/{page:int?}/{search?}")]
+		public IActionResult Index(string search, int? page) {
+			SearchModel searchModel = new SearchModel { search = search };
+			if (String.IsNullOrWhiteSpace(search)) {
+				searchModel = SearchDefaultResult(searchModel, page);
+				return View(searchModel);
+			}
+
+			searchModel = GetCertificates(searchModel, page);
+			return View(searchModel);
+		}
+
+		public SearchModel SearchDefaultResult(SearchModel searchModel, int? currentPage) {
+			var searchResult = certificateHandler.GetCertificatesPagesResultAmount();
+			searchModel.resultPages = (int)Math.Ceiling(Decimal.Divide(searchResult, resultsPerPage));
+
+			//TODO
+			//REMOVE CW
+			Console.WriteLine($"Divide and round: {searchResult},{resultsPerPage}");
+			Console.WriteLine($"Result pages: {searchModel.resultPages}");
+
+			if (currentPage.HasValue) {
+				searchModel.certificatesResult = certificateHandler.GetCertificatesPages(resultsPerPage, currentPage.Value);
+			} else {
+				searchModel.certificatesResult = certificateHandler.GetCertificatesPages(resultsPerPage, 1);
+			}
+			return searchModel;
+		}
+
+		public SearchModel GetCertificates(SearchModel searchModel, int? page) {
+			int currentPage = 1;
+			if (page.HasValue) {
+				currentPage = page.Value;
+			}
+			int searchResult = certificateHandler.GetCertificatesPagesResultAmount(searchModel.search);
+			searchModel.resultPages = (int)Math.Ceiling(Decimal.Divide(searchResult, resultsPerPage));
+
+			Console.WriteLine($"Divide and round: {searchResult},{resultsPerPage}");
+			Console.WriteLine($"Result pages: {searchModel.resultPages}");
+			//TODO
+			//REMOVE CW
+
+			searchModel.certificatesResult = certificateHandler.GetCertificatesPages(resultsPerPage, currentPage, searchModel.search);
+			return searchModel;
 		}
 	}
 }
