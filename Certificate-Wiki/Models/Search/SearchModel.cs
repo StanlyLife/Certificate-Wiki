c697edc3daba208781554a01a9285962df0323f3
Stian have <stianhave@hotmail.com>
2020-06-15 21:01:32 +0200

    Added search model

---
+++ Certificate-Wiki/Models/Search/SearchModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.Search {
+
+	public class SearchModel {
+
+		[BindProperty(SupportsGet = true)]
+		public string search { get; set; }
+
+		public IEnumerable<Certificates> certificatesResult { get; set; }
+
+		public int resultPages { get; set; }
+	}
+}
