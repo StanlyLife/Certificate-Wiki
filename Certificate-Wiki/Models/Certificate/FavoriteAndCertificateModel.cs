43cc33c214a4df8a0489fe3580a043d5f86a5aa2
Stian have <stianhave@hotmail.com>
2020-06-17 15:26:07 +0200

    Implemented new model The purpose of the model is to carry information whether or not the certificate is favorited or not

---
+++ Certificate-Wiki/Models/Certificate/FavoriteAndCertificateModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.Certificate {
+
+	public class FavoriteAndCertificateModel {
+
+		public FavoriteAndCertificateModel() {
+			counter = 0;
+			isFavorite = new List<bool>();
+		}
+
+		public IEnumerable<Certificates> certificate { get; set; }
+
+		public List<bool> isFavorite { get; set; }
+
+		public int counter { get; set; }
+	}
+}
