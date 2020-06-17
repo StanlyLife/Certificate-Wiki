c7b57bccb0831f02daea4528979458259dcb06ef
Stian have <stianhave@hotmail.com>
2020-06-17 19:11:47 +0200

    Added ViewModel for users when displayed

---
+++ Certificate-Wiki/Models/Certificate/ProfileModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.Certificate {
+
+	public class ProfileModel {
+
+		public ProfileModel() {
+			facm = new FavoriteAndCertificateModel();
+		}
+		public FavoriteAndCertificateModel facm { get; set; }
+
+		public CertificateUser profile { get; set; }
+		public string profilePicture { get; set; }
+	}
+}
