164efadd1acb734fd4c046ad06ef68d1b245b7d1
Stian have <stianhave@hotmail.com>
2020-06-17 17:23:20 +0200

    Implemented search logic Using linq and EF a query search for users where the username contains the search parameter will be executed

---
+++ Certificate-Wiki/Interface/IUserSearch.cs
@@ -0,0 +1,13 @@
+﻿using Certificate_Wiki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Interface {
+
+	public interface IUserSearch {
+
+		public IEnumerable<CertificateUser> SearchUserName(string userName);
+	}
+}
---
+++ Certificate-Wiki/Interface/Implementation/UserSearch.cs
@@ -0,0 +1,28 @@
+﻿using Certificate_Wiki.Data;
+using Certificate_Wiki.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Interface.Implementation {
+
+	public class UserSearch : IUserSearch {
+		private readonly CertificateDbContext db;
+		private readonly UserManager<CertificateUser> userManager;
+
+		public UserSearch(CertificateDbContext db, UserManager<CertificateUser> userManager) {
+			this.db = db;
+			this.userManager = userManager;
+		}
+
+		public IEnumerable<CertificateUser> SearchUserName(string userName) {
+			var query = from entity in db.Users
+						where entity.NormalizedUserName.Contains(userName)
+						select entity;
+
+			return query;
+		}
+	}
+}
