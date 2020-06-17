f34ea6b7fc0c33222950b6f0857a7bcfab153de1
Stian have <stianhave@hotmail.com>
2020-06-17 17:24:27 +0200

    Added dependency injection to IUserSearch

--- Certificate-Wiki/Startup.cs
+++ Certificate-Wiki/Startup.cs
@@ -31,6 +31,7 @@ namespace Certificate_Wiki {
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddScoped<ICertificateHandler, CertificateHandler>();
 			services.AddScoped<IFavoriteHandler, FavoriteHandler>();
+			services.AddScoped<IUserSearch, UserSearch>();
 			services.AddControllersWithViews();
 
 			services.AddRazorPages()
