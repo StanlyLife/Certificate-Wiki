117262790325462078b8e87178fa91dc66804bc7
Stian have <stianhave@hotmail.com>
2020-06-18 23:47:49 +0200

    Improved UI for forms Active forms now had a red bottom border

---
+++ Certificate-Wiki/wwwroot/js/FormInputs.js
@@ -0,0 +1,16 @@
+﻿const inputs = document.querySelectorAll(".text-input");
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", AddBorder);
+  input.addEventListener("focusout", RemoveBorder);
+});
+
+function AddBorder(e) {
+  e.target.classList.add("input-border");
+}
+
+function RemoveBorder(e) {
+  e.target.classList.remove("input-border");
+}
+
+console.log("loaded forminput.js");
