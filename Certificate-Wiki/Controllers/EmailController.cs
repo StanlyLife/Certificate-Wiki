ab9a98cb6d923988f3119616c564b0c1947e2cd1
Stian have <stianhave@hotmail.com>
2020-06-20 19:22:58 +0200

    Added temporary authorization for emails

--- Certificate-Wiki/Controllers/EmailController.cs
+++ Certificate-Wiki/Controllers/EmailController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Certificate_Wiki.Models.Email;
 using Certificate_Wiki.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Certificate_Wiki.Controllers {
 
@@ -15,11 +17,14 @@ namespace Certificate_Wiki.Controllers {
 			_emailSender = emailSender;
 		}
 
+		[Authorize(Policy = "Owner")]
 		[HttpGet]
 		public IActionResult SendEmail() {
 			return View();
 		}
 
+		[Authorize(Policy = "Owner")]
+		[ValidateAntiForgeryToken]
 		[HttpPost]
 		public async Task<ViewResult> SendEmail(EmailModel model) {
 			if (ModelState.IsValid) {
