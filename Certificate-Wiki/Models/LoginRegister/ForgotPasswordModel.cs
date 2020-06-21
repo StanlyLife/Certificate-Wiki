34ee9664b8d8f723e7cd8fd3425fd6d65f09f54e
Stian have <stianhave@hotmail.com>
2020-06-21 16:43:03 +0200

    Added model to support information transaction to password reset

---
+++ Certificate-Wiki/Models/LoginRegister/ForgotPasswordModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.LoginRegister {
+
+	public class ForgotPasswordModel {
+		public string userName { get; set; }
+
+		public string email { get; set; }
+	}
+}
---
+++ Certificate-Wiki/Models/LoginRegister/ResetPasswordModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate_Wiki.Models.LoginRegister {
+
+	public class ResetPasswordModel {
+
+		[Required(ErrorMessage = "Something went wrong! Cannot find email.")]
+		public string email { get; set; }
+
+		[Required(ErrorMessage = "Something went wrong! Cannot find token.")]
+		public string token { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare("confirmPassword", ErrorMessage = "Passwords does not match")]
+		public string password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string confirmPassword { get; set; }
+	}
+}
